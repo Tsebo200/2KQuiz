031e50cb642d75fa47f595e0f9e7560291fa20e7
Tsebo Rams <200200@virtualwindow.co.za>
2022-08-28 07:42:01 +0200

    Welcome Page is activity_main.xml and It has been styled, add Image though

--- app/src/main/java/com/example/a2kquiz/MainActivity.kt
+++ app/src/main/java/com/example/a2kquiz/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.a2kquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding = ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
