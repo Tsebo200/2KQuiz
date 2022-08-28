6b4d8b4185dd3b37a70c99ec557373dada4d45cb
Tsebo Rams <200200@virtualwindow.co.za>
2022-08-28 06:39:20 +0200

    Laerngt About Binding

--- app/src/main/java/com/example/a2kquiz/QuestionActivity.kt
+++ app/src/main/java/com/example/a2kquiz/QuestionActivity.kt
@@ -2,12 +2,26 @@ package com.example.a2kquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.a2kquiz.databinding.ActivityQuestionBinding
+import com.example.a2kquiz.models.Constants.getAllQuestions
 
 class QuestionActivity : AppCompatActivity() {
+    //We need to define binding view
+    private lateinit var binding: ActivityQuestionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_question)
+        //UPDATED: inflate
+        binding = ActivityQuestionBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         //Call for all my questions
+        var questions = getAllQuestions()
+        Log.i("Question Count:", "${questions[0].questionText}")
+
+        binding.tvQuestionText.setText(questions[3].questionText)
+
+//        R.id.tv_question_text.setText(questions[0].questionText)
     }
 }
--- app/src/main/java/com/example/a2kquiz/models/Constants.kt
+++ app/src/main/java/com/example/a2kquiz/models/Constants.kt
@@ -4,12 +4,12 @@ import com.example.a2kquiz.R
 
 object Constants {
     //This function will return all questions
-    fun getAllQuestions():ArrayList<Question>{
-     //1. Create Empty ArrayList
+    fun getAllQuestions(): ArrayList<Question> {
+        //1. Create Empty ArrayList
         var allQuestions = ArrayList<Question>()
 
         //TODO: 2. Create data
-        val que1 = Question(
+        val Marvque1 = Question(
             1,
             "Which Spider-Man was in No Way Home",
             R.drawable.ic_help,
@@ -18,12 +18,65 @@ object Constants {
             optionThree = "Toby Maguire",
             optionFour = "All Of The Above"
         )
+
+        //TODO: Add Marvel Questions
+        val Marvque2 = Question(
+            2,
+            "Who is the new Captain America",
+            R.drawable.ic_help,
+            "Loki Laufeyson (Loki)",
+            "Sam Wilson (Falcon)",
+            "Bucky Barnes (The Winter Soldier)",
+            "Peggy Carter"
+        )
+
+        //TODO: Add Anime Questions
+        val Anique1 = Question(
+            1,
+            "Ilike Pixxa",
+            R.drawable.ic_help,
+            "Loki Laufeyson (Loki)",
+            "Sam Wilson (Falcon)",
+            "Bucky Barnes (The Winter Soldier)",
+            "Peggy Carter"
+        )
+
+
+        //TODO: Add Formula One Questions
+        val Formque1 = Question(
+            1,
+            "Who is You",
+            R.drawable.ic_help,
+            "Loki Laufeyson (Loki)",
+            "Sam Wilson (Falcon)",
+            "Bucky Barnes (The Winter Soldier)",
+            "Peggy Carter"
+        )
+
+        //TODO: Add NBA Questions
+        val NBAque1 = Question(
+            1,
+            "Lets play",
+            R.drawable.ic_help,
+            "Loki Laufeyson (Loki)",
+            "Sam Wilson (Falcon)",
+            "Bucky Barnes (The Winter Soldier)",
+            "Peggy Carter"
+        )
+
         //add question to array
-        allQuestions.add(que1)
+        allQuestions.add(Marvque1)
+        allQuestions.add(Marvque2)
+        allQuestions.add(Anique1)
+        allQuestions.add(Formque1)
+        allQuestions.add(NBAque1)
         //3. return data
         return allQuestions
 
-    //TODO: Add Marvel Questions
+
     }
 
+
 }
+
+
