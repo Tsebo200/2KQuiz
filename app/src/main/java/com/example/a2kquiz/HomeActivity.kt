026e589a57cf89d29634a5c62f7fba1fcd98762b
Tsebo Rams <200200@virtualwindow.co.za>
2022-08-28 08:55:08 +0200

    Just Before Home Page Restart

---
+++ app/src/main/java/com/example/a2kquiz/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a2kquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a2kquiz.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/example/a2kquiz/models/Home.kt
@@ -0,0 +1,13 @@
+package com.example.a2kquiz.models
+//
+//import android.os.Bundle
+//import com.example.a2kquiz.databinding.h
+//
+//class Home {
+//    private lateinit var binding: h
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)//This sets what xml frontend to use
+//    }
+//}
