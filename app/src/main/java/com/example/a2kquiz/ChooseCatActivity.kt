1c733b2a130a43594b9e3f56b44050a2243eba34
Tsebo Rams <200200@virtualwindow.co.za>
2022-08-29 20:42:33 +0200

    Added Choose Category Activity

---
+++ app/src/main/java/com/example/a2kquiz/ChooseCatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2kquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseCatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_cat)
+    }
+}
